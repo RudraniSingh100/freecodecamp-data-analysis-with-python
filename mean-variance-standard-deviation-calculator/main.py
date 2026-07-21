1d64f9bbf5f0de6e85b52e30d7d5a57d6d75757c
Rudrani <165304017+RudraniSingh100@users.noreply.github.com>
2026-07-21 19:25:30 +0530

    Add main.py to demonstrate calculate function

---
+++ mean-variance-standard-deviation-calculator/main.py
@@ -0,0 +1,9 @@
+from mean_var_std import calculate
+
+print("Example 1")
+print(calculate([0, 1, 2, 3, 4, 5, 6, 7, 8]))
+
+print()
+
+print("Example 2")
+print(calculate([2, 6, 2, 8, 4, 0, 1, 5, 7]))
